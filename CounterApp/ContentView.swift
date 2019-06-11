cc738c2c6323131b8b8509cb0f34ed909c1828f2
Stan Lemon <stanlemon@users.noreply.github.com>
2019-06-11 15:09:21 -0400

    Initial Commit

---
+++ CounterApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CounterApp
+//
+//  Created by Stan Lemon on 6/11/19.
+//  Copyright © 2019 Stan Lemon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
